4e1c26287ec9a35e2e1f5eb6b1a6150979c06b3c
mamun060 <almamun.raju520@gmail.com>
2026-04-12 22:51:34 +0600

    promises and promise.all and promise.race

---
+++ asynchronous/promise/promiseRace.js
@@ -0,0 +1,12 @@
+const user = fetch('https://jsonplaceholder.typicode.com/users/1').then(res => res.json());
+const post = fetch('https://jsonplaceholder.typicode.com/posts/1').then(res => res.json());
+const comment = fetch('https://jsonplaceholder.typicode.com/comments/1').then(res => res.json());
+
+Promise.race([user, post, comment])
+.then((response)=>{
+    console.log(response);
+})
+.catch((error)=>{
+    console.log(error);
+})
+
